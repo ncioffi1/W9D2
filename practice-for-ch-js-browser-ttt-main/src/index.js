2f782254fef806f0bcad60c6fe56c01ceae1eb7a
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-12-12 17:16:17 -0500

    Finish Tic Tac Toe

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,11 @@
-Add your import statements for View and Game here
-
+// Add your import statements for View and Game here
+const View = require("./ttt-view.js")
+const Game = require("../ttt_node/game.js")
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  let game =new Game()
+  let el = document.querySelector(".ttt")
+  let header = document.querySelector(".gameHappening")
+  let view = new View(game, el , header)
+  view.setupBoard()
+  
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,18 +1,69 @@
+// const game = require("../ttt_node/game.js")
 class View {
-  constructor(game, el) {
+  constructor(game, el, header) {
+    this.game = game 
+    this.el = el
+    this.header = header
+   this.handleClick = this.handleClick.bind(this)
+   this.handleGameOver = this.handleGameOver.bind(this)
   }
   
   setupBoard() {
+    let ul = document.createElement('ul')
+  
+    for(let i = 0; i< 3; i++){
+      for(let j = 0; j < 3; j++){
+
+        let li = document.createElement('li')
+        li.dataset.pos = [i,j]
+        
+        li.addEventListener("mouseover", this.handleHover)
+        li.addEventListener("mouseout", this.handleHover)
+        // li.dataset.y = j
+        ul.appendChild(li)
+      }
+    }
+    this.el.appendChild(ul)
+    ul.addEventListener("click", this.handleClick)
+  }
+
+ handleHover(e){
+    e.stopPropagation()
+    let ele = e.target 
+    ele.classList.toggle('done')
   }
   
   handleClick(e) {
+    if(!this.game.isOver()){
+  
+    if(!e.target.classList.contains('clicked')){
+
+      e.stopPropagation()
+      let ele = e.target 
+      ele.classList.toggle('clicked')
+    }
+    e.target.textContent = this.game.currentPlayer
+    if(this.game.currentPlayer === 'x'){
+      e.target.classList.toggle('x')
+    }else{
+      e.target.classList.toggle('o')
+    }
+    this.game.playMove(e.target.dataset.pos.split(",").map(el=> parseInt(el)))
+    this.handleGameOver()
+  } 
   }
 
-  makeMove(square) {
-  }
   
   handleGameOver() {
+    if(this.game.isOver()){
+      // this.game.winner
+      if(this.game.winner() === 'x' || this.game.winner() === 'o'){
+        this.header.textContent = this.game.winner() + " is winner!"
+      }else{
+        this.header.textContent = 'Draw!'
+      }
+    }
   }
 }
 
-export default View;
+module.exports = View
